65c40d9cadb1e6dd5a10611fc58fa6e267d965c3
polsust <polsust@gmail.com>
2023-04-26 21:43:34 +0200

    Fix test-package auto change page

--- test-package/index.ts
+++ test-package/index.ts
@@ -3,7 +3,7 @@ import { ExplodingTopicsScraper } from "exploding-topics-scraper";
 (async () => {
   let page = 1;
   while (true) {
-    console.log(await ExplodingTopicsScraper.getTopics(1));
+    console.log(await ExplodingTopicsScraper.getTopics(page));
     page++;
   }
 })();
